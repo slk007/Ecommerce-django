5bb26ce8bdccd668fa2864160bd0a9fe3dc11c39
slk007 <shubham.soni.lko@gmail.com>
2022-02-04 18:34:03 +0530

    fetch data from backend API's, using axios, use CORS headers

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 
 import Rating from "../components/Rating";
-import products from "../products";
 
-function ProductScreen() {
+function ProductScreen({ match }) {
   const { id } = useParams();
 
-  const product = products.find((p) => p._id === id);
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    }
+
+    fetchProduct();
+  }, []);
 
   return (
     <div>
