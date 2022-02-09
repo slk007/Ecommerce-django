81fd4ab5e0f4ab7d997a7d27ff3c93699c179968
slk007 <shubham.soni.lko@gmail.com>
2022-02-09 16:15:55 +0530

    add Redux in HomeScreen for Product LIst

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -4,13 +4,13 @@ import {
   PRODUCT_LIST_FAIL,
 } from "../constants/productConstants";
 
-export const productListReducer = (state = { prouducts: [] }, action) => {
+export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-      return { loading: true, prouducts: [] };
+      return { loading: true, products: [] };
 
     case PRODUCT_LIST_SUCCESS:
-      return { loading: false, prouducts: action.payload };
+      return { loading: false, products: action.payload };
 
     case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,26 +1,24 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 
 import Product from "../components/Product";
+import { listProducts } from "../actions/productActions";
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products } = productList;
 
   useEffect(() => {
-    async function fetchProducts() {
-      const { data } = await axios.get("/api/products/");
-      setProducts(data);
-    }
-
-    fetchProducts();
+    dispatch(listProducts());
   }, []);
 
   return (
     <div>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product) => (
+        {products?.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
